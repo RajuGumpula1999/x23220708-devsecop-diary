36a4a8b80d2186a8e98a2548807f6acbc7c56aab
RajuGumpula1999 <x23220708@student.ncirl.ie>
2024-04-09 09:37:51 +0100

    changes of dairy

--- dairy/asgi.py
+++ dairy/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Notebook project.
+ASGI config for dairy project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Notebook.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dairy.settings')
 
 application = get_asgi_application()
--- dairy/wsgi.py
+++ dairy/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Notebook project.
+WSGI config for dairy project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Notebook.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dairy.settings')
 
 application = get_wsgi_application()
